16d56c38f6112db49d7c0e546200af481bfb48d7
tjquinn1 <tjquinn1@asu.edu>
2020-12-18 11:22:27 -0700

    fixed commits

--- place_resolver.py
+++ place_resolver.py
@@ -3,6 +3,7 @@ import sys, getopt
 import json
 import re
 import logging
+import math
 from place_reader import PlaceReader
 
 SILENT = True
@@ -76,6 +77,58 @@ class PlaceResolver:
 
         return place_entries
 
+    def process_length_3_coords(coords):
+        d = coords[0]
+        m = coords[1]
+        se = coords[2]
+        dd = 0;
+        if d == 0:
+            dd = ((m / 60.0) + (se / 3600.0));
+        else:
+            # python doesn't have a clean way to get the symbol of a number so we use copysign
+            dd = math.copysign(1,d) * abs(d) + (m / 60.0) + (se / 3600.0)
+        return round(dd, 6)
+
+    def process_length_2_coords(coords):
+        d = coords[0]
+        m = coords[1]
+        return round(math.copysign(1,d) * abs(d) + (m / 60.0), 6)
+
+    def clean_coordinates(coord):
+        split_string =  coord.split("|")
+        coords = []
+        formatted_coords = []
+        for string in split_string:
+            try:
+                # check if string is a int/float and cast to float
+                coords.append(float(string))
+            except:
+                pass
+            if string in ["N", "E"]:
+                if len(coords) == 3:
+                    formatted_coords.append(process_length_3_coords(coords))
+                    coords.clear()
+                if len(coords) == 2:
+                    formatted_coords.append(clean_coordinates(coord))
+                    coords.clear()
+                if len(coords) == 1:
+                    formatted_coords.append(coords[0])
+                    coords.clear()
+            elif string in ["S", "W"]:
+                # handle negative coords
+                if len(coords) == 3:
+                    formatted_coords.append(process_length_3_coords(coords) * -1)
+                    coords.clear()
+                if len(coords) == 2:
+                    formatted_coords.append(clean_coordinates(coord) * -1)
+                    coords.clear()
+                if len(coords) == 1:
+                    formatted_coords.append(coords[0] * -1)
+                    coords.clear()
+        if coords:
+            return coords
+        return formatted_coords
+
     def resolve_place(self, place_name):
         entries = self.find_in_title(place_name)
         potential_place_entries = self.filter_place_entries(entries)
@@ -88,7 +141,8 @@ class PlaceResolver:
             result.update({
                 'wikipedia_entry_title': potential_place_entries[0]['title'],
                 'coodinates': potential_place_entries[0]['coordinates'],
-                'wikipedia_entry_url': "https://en.wikipedia.org/wiki/" + potential_place_entries[0]['title']
+                'wikipedia_entry_url': "https://en.wikipedia.org/wiki/" + potential_place_entries[0]['title'],
+                'cleaned_coordinates': self.clean_coordinates(potential_place_entries[0]['coordinates'])
             })
             log("INFO: Selecting: '{}' at {}".format(potential_place_entries[0]['title'], potential_place_entries[0]['coordinates']), always=True)
 
